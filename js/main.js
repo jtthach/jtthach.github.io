8aea6d231e22f7cfee37598d446ac5be559b1b6d
jtthach <jtthach@uci.edu>
2016-11-08 18:28:55 -0800

    New

---
+++ js/main.js
@@ -0,0 +1,34 @@
+
+function main(){
+	//fade for some WACKY effects
+	$('.small_pic').hide();
+	$('.small_pic').fadeIn(500);
+};
+$(document).ready(main);
+
+
+// Get the modal
+$(document).ready(function(){
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementsByClassName('SQL');
+console.log(img);
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+for(var i = 0; i<img.length; ++i)
+ img[i].onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+});
